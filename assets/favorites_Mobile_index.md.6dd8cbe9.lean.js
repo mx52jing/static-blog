087f12d37dabe28cb17366a060ed0f193c915dc0
mx52jing <945972621@qq.com>
2023-10-30 15:07:54 +0000

    Deploying to gh-pages from @ mx52jing/static-blog@e68e43feebfae2f031f9eca7a874078e014373ae 🚀

---
+++ assets/favorites_Mobile_index.md.6dd8cbe9.lean.js
@@ -0,0 +1,1 @@
+import{_ as e,c as r,o as t,a}from"./app.aaa56bb0.js";const u=JSON.parse('{"title":"Mobile Side","description":"","frontmatter":{"layout":"doc"},"headers":[],"relativePath":"favorites/Mobile/index.md"}'),i={name:"favorites/Mobile/index.md"},o=a("",2),l=[o];function n(s,h,_,c,d,p){return t(),r("div",null,l)}const b=e(i,[["render",n]]);export{u as __pageData,b as default};
