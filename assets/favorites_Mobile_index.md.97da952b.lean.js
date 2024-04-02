a49e15778c53f1879b2730685bdd35acbf1c7505
mx52jing <22701464+mx52jing@users.noreply.github.com>
2024-04-02 14:41:31 +0000

    Deploying to gh-pages from @ mx52jing/static-blog@4ee452785a83533767a333e5ace989cc0d10af64 🚀

---
+++ assets/favorites_Mobile_index.md.97da952b.lean.js
@@ -0,0 +1,1 @@
+import{_ as e,c as r,o as t,a}from"./app.0a5c07a3.js";const u=JSON.parse('{"title":"Mobile Side","description":"","frontmatter":{"layout":"doc"},"headers":[],"relativePath":"favorites/Mobile/index.md"}'),i={name:"favorites/Mobile/index.md"},o=a("",2),l=[o];function n(s,h,_,c,d,p){return t(),r("div",null,l)}const b=e(i,[["render",n]]);export{u as __pageData,b as default};
