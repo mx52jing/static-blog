e01d71eb5897df3d37f720cdfcc54a6b77739221
mx52jing <22701464+mx52jing@users.noreply.github.com>
2024-06-13 09:06:32 +0000

    Deploying to gh-pages from @ mx52jing/static-blog@fb314898c90a976b9c63e9c07bdace2bf499fa0e 🚀

---
+++ assets/favorites_Nginx_index.md.75bdb2ab.lean.js
@@ -0,0 +1,1 @@
+import{_ as e,c as t,o as n,e as r}from"./app.d034dc03.js";const p=JSON.parse('{"title":"Nginx","description":"","frontmatter":{"layout":"doc"},"headers":[],"relativePath":"favorites/Nginx/index.md"}'),a={name:"favorites/Nginx/index.md"},i=r("",2),o=[i];function s(_,c,l,d,x,h){return n(),t("div",null,o)}const g=e(a,[["render",s]]);export{p as __pageData,g as default};
